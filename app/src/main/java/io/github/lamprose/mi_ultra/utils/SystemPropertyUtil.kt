d149851d7ea2c73258af43d4526c077adeb81dad
lamprose <2316892579@qq.com>
2021-10-27 09:53:47 +0800

    refactor(代码): 整理代码2
    [skip ci]

--- app/src/main/java/io/github/lamprose/mi_ultra/utils/SystemPropertyUtil.kt
+++ app/src/main/java/io/github/lamprose/mi_ultra/utils/SystemPropertyUtil.kt
@@ -1,43 +1,42 @@
-package io.github.lamprose.mi_ultra.utils;
+package io.github.lamprose.mi_ultra.utils
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Properties;
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.*
 
 /**
  * Android 系统属性读取工具类
  * Created by Zhuliya on 2018/10/22
  */
-public class SystemPropertyUtil {
-
+object SystemPropertyUtil {
     /**
      * 使用命令方式读取系统属性
      *
      * @param propName propName
      * @return String
      */
-    public static String getSystemProperty(String propName) {
-        String line;
-        BufferedReader input = null;
+    fun getSystemProperty(propName: String): String? {
+        val line: String
+        var input: BufferedReader? = null
         try {
-            Process p = Runtime.getRuntime().exec("getprop " + propName);
-            input = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
-            line = input.readLine();
-            input.close();
-        } catch (IOException ex) {
+            val p = Runtime.getRuntime().exec("getprop $propName")
+            input = BufferedReader(InputStreamReader(p.inputStream), 1024)
+            line = input.readLine()
+            input.close()
+        } catch (ex: IOException) {
             //Log.e("Unable to read sysprop " + propName, ex);
-            return null;
+            return null
         } finally {
             if (input != null) {
                 try {
-                    input.close();
-                } catch (IOException e) {
+                    input.close()
+                } catch (e: IOException) {
                     //Log.e("Exception while closing InputStream", e);
                 }
             }
         }
-        return line;
+        return line
     }
 
     /**
@@ -45,14 +44,15 @@ public class SystemPropertyUtil {
      *
      * @return Prop
      */
-    public static Properties getProperty() {
-        Properties properties = new Properties();
-        try {
-            Process p = Runtime.getRuntime().exec("getprop");
-            properties.load(p.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
+    val property: Properties
+        get() {
+            val properties = Properties()
+            try {
+                val p = Runtime.getRuntime().exec("getprop")
+                properties.load(p.inputStream)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return properties
         }
-        return properties;
-    }
 }
